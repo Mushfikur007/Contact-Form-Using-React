ec1a13ead3ea072569fbd52c9253225c51b1bf19
Mushfikur Rahman <mushfikurrahman1624@gmail.com>
2024-02-26 21:10:40 +0600

    nav bar

---
+++ vite-project/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+const nav = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="./img " alt="" />
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default nav;
